f93313345a08c222df8343c4a5cd33689ba7b24a
Ouzyy <dioufousmaneeee@gmail.com>
2025-04-25 14:41:29 +0000

    commit

---
+++ src/app/adminPage/agences/agences.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agences',
+  standalone: true,
+  imports: [],
+  templateUrl: './agences.component.html',
+  styleUrl: './agences.component.scss'
+})
+export class AgencesComponent {
+
+}
