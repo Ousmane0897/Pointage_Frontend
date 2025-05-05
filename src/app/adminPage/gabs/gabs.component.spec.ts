a88208fccd56abcec08b0b90bdbe1478c333f2b7
Ousmane0897 <dioufousmaneeee@gmail.com>
2025-05-05 16:16:01 +0000

    pointage

---
+++ src/app/adminPage/gabs/gabs.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GabsComponent } from './gabs.component';
+
+describe('GabsComponent', () => {
+  let component: GabsComponent;
+  let fixture: ComponentFixture<GabsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GabsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(GabsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/adminPage/gabs/gabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './gabs.component.html',
+  styleUrl: './gabs.component.scss'
+})
+export class GabsComponent {
+
+}
